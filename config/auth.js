694ff61b5aa2f6e79e49aa2dbb9f3e9fb6878cd0
Arpit <apmeena786@gmail.com>
2017-10-15 00:37:09 +0530

    Arpit | Social login config changed

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '198849700658030', // your App ID
         'clientSecret'    : '5e36e9dca0e41e56362c17e85f479be5', // your App Secret
-        'callbackURL'     : 'http://localhost:3000/auth/facebook/callback',
+        'callbackURL'     : 'https://learningapp-dev.herokuapp.com/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.10/me?fields=first_name,last_name,email'
 
     },
@@ -14,13 +14,13 @@ module.exports = {
     'twitterAuth' : {
         'consumerKey'        : 'your-consumer-key-here',
         'consumerSecret'     : 'your-client-secret-here',
-        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'        : 'https://learningapp-dev.herokuapp.com/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'         : '693095659675-htv4tvtpe7erg8f3qro4g7f0054ljtlf.apps.googleusercontent.com',
         'clientSecret'     : 'qIQKEgPnu5dS1iMU68q93fMA',
-        'callbackURL'      : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'      : 'https://learningapp-dev.herokuapp.com/auth/google/callback'
         // reference: https://console.developers.google.com/apis/api/plus.googleapis.com/overview?project=693095659675
     }
 
